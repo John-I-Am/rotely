baad044ef17f709b8a156ae7b03c3399e400a8ce
John <47208192+John-I-Am@users.noreply.github.com>
2025-09-22 10:43:02 +1200

    feat(mantine): add mantine UI library

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,48 +1,54 @@
 /// <reference types="vite/client" />
-import type {ReactNode} from 'react'
-import {
-  Outlet,
-  createRootRoute,
-  HeadContent,
-  Scripts,
-} from '@tanstack/react-router'
 
-const RootDocument = ({ children}: Readonly<{ children: ReactNode}>) => {
-    return (
-    <html>
-      <head>
-        <HeadContent />
-      </head>
-      <body>
-        {children}
-        <Scripts />
-      </body>
-    </html>
-  )
-}
+import "@mantine/core/styles.css";
+
+import { MantineProvider, mantineHtmlProps } from "@mantine/core";
+import {
+	createRootRoute,
+	HeadContent,
+	Outlet,
+	Scripts,
+} from "@tanstack/react-router";
+
+import type { ReactNode } from "react";
+import { theme } from "@/theme";
+
+const RootDocument = ({ children }: Readonly<{ children: ReactNode }>) => {
+	return (
+		<html {...mantineHtmlProps}>
+			<head>
+				<HeadContent />
+			</head>
+			<body>
+				<MantineProvider theme={theme}> {children} </MantineProvider>
+				<Scripts />
+			</body>
+		</html>
+	);
+};
 
 const RootComponent = () => {
-  return (
-    <RootDocument>
-      <Outlet />
-    </RootDocument>
-  )
-}
+	return (
+		<RootDocument>
+			<Outlet />
+		</RootDocument>
+	);
+};
 
 export const Route = createRootRoute({
-  head: () => ({
-    meta: [
-      {
-        charSet: 'utf-8',
-      },
-      {
-        name: 'viewport',
-        content: 'width=device-width, initial-scale=1',
-      },
-      {
-        title: 'TanStack Start Starter',
-      },
-    ],
-  }),
-  component: RootComponent,
-})
+	head: () => ({
+		meta: [
+			{
+				charSet: "utf-8",
+			},
+			{
+				name: "viewport",
+				content: "width=device-width, initial-scale=1",
+			},
+			{
+				title: "TanStack Start Starter",
+			},
+		],
+	}),
+	component: RootComponent,
+});
---
+++ src/theme.ts
@@ -0,0 +1,103 @@
+import {
+	ActionIcon,
+	Button,
+	colorsTuple,
+	createTheme,
+	Group,
+	Image,
+	Loader,
+	Paper,
+	rem,
+	ScrollArea,
+	Skeleton,
+	Textarea,
+	TextInput,
+	Tooltip,
+} from "@mantine/core";
+
+export const theme = createTheme({
+	colors: {
+		"dark-navy": colorsTuple("#2c3143"),
+	},
+
+	black: "#2c3143",
+	primaryColor: "indigo",
+	primaryShade: { light: 5, dark: 9 },
+
+	fontFamily: "notoSans, sans-serif",
+	headings: {
+		fontFamily: "notoSans, sans-serif",
+		fontWeight: "500",
+		sizes: {
+			h1: { fontSize: rem(22) },
+			h2: { fontSize: rem(20) },
+			h3: { fontSize: rem(18) },
+		},
+	},
+
+	components: {
+		Group: Group.extend({
+			defaultProps: {
+				wrap: "nowrap",
+			},
+		}),
+		Button: Button.extend({
+			defaultProps: {
+				color: "dark-navy",
+				radius: "md",
+				loaderProps: { type: "dots" },
+			},
+		}),
+		ActionIcon: ActionIcon.extend({
+			defaultProps: {
+				radius: "md",
+				loaderProps: { type: "dots" },
+			},
+		}),
+		Paper: Paper.extend({
+			defaultProps: {
+				radius: "md",
+				p: "md",
+			},
+		}),
+		Tooltip: Tooltip.extend({
+			defaultProps: {
+				openDelay: 1000,
+			},
+		}),
+		TextInput: TextInput.extend({
+			defaultProps: {
+				radius: "md",
+			},
+			styles: { error: { position: "absolute" } },
+		}),
+		Textarea: Textarea.extend({
+			defaultProps: {
+				radius: "md",
+			},
+			styles: { error: { position: "absolute" } },
+		}),
+		Skeleton: Skeleton.extend({
+			defaultProps: {
+				radius: "md",
+			},
+		}),
+		ScrollArea: ScrollArea.extend({
+			defaultProps: {
+				offsetScrollbars: true,
+				type: "always",
+				h: "100%",
+			},
+		}),
+		Image: Image.extend({
+			defaultProps: {
+				radius: "md",
+			},
+		}),
+		Loader: Loader.extend({
+			defaultProps: {
+				type: "dots",
+			},
+		}),
+	},
+});
