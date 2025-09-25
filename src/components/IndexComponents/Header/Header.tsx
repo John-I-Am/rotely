f082132531ceb4c704136d70dd1d3541cae3acaf
John <47208192+John-I-Am@users.noreply.github.com>
2025-09-25 12:56:46 +1200

    feat(home): add link to login page

--- src/components/IndexComponents/Header/Header.tsx
+++ src/components/IndexComponents/Header/Header.tsx
@@ -1,14 +1,23 @@
-import { Group } from "@mantine/core";
-
+import { Button, Group } from "@mantine/core";
+import { Link } from "@tanstack/react-router";
 import { Logo } from "@/components/Logo/Logo";
 import { ThemeSwitch } from "@/components/ThemeSwitch/ThemeSwitch";
+import { Route as LoginRoute } from "@/routes/login";
 
 export const Header = () => {
 	return (
 		<header>
 			<Group p="md" justify="space-between">
 				<Logo />
-				<ThemeSwitch />
+
+				<Group>
+					<Group>
+						<ThemeSwitch />
+					</Group>
+					<Button ml="lg" component={Link} from="/" to={LoginRoute.to}>
+						Login
+					</Button>
+				</Group>
 			</Group>
 		</header>
 	);
