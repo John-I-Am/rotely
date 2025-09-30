4042f3ccfb8af4bef1344895f8eb7b395650490c
John <47208192+John-I-Am@users.noreply.github.com>
2025-09-30 14:48:04 +1300

    feat(ui): add placeholder dashboard grid layout

--- src/routes/app/dashboard.tsx
+++ src/routes/app/dashboard.tsx
@@ -1,9 +1,25 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { SimpleGrid, Skeleton, Stack } from "@mantine/core";
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+import type { Deck } from "@/generated/prisma/client";
 
-const RouteComponent = () => {
-	return <div style={{ border: "1px solid red" }}>Hello "/app/dashboard"!</div>;
+const Dashboard = () => {
+	const PRIMARY_COL_HEIGHT = "300px";
+	const SECONDARY_COL_HEIGHT = `calc(${PRIMARY_COL_HEIGHT} / 2 - var(--mantine-spacing-md) / 2)`;
+
+	const router = useRouter();
+	const decks: Deck[] = Route.useLoaderData();
+
+	return (
+		<SimpleGrid cols={{ base: 1, sm: 2 }} w="100%">
+			<Skeleton height={PRIMARY_COL_HEIGHT} radius="md" animate={false} />
+			<Stack>
+				<Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+				<Skeleton height={SECONDARY_COL_HEIGHT} radius="md" animate={false} />
+			</Stack>
+		</SimpleGrid>
+	);
 };
 
 export const Route = createFileRoute("/app/dashboard")({
-	component: RouteComponent,
+	component: Dashboard,
 });
