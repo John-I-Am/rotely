e65a291b810b2d21e2fa3a47db6f6805a1a39ac9
John <47208192+John-I-Am@users.noreply.github.com>
2025-10-08 00:05:38 +1300

    feat(cardTable): add table to display cards

--- src/features/cards/components/CardTable/CardTable.tsx
+++ src/features/cards/components/CardTable/CardTable.tsx
@@ -1,27 +1,129 @@
-import { Button, Group } from "@mantine/core";
+import { Box, Button, Checkbox, Stack, Table, Text } from "@mantine/core";
+import cx from "clsx";
+import { useState } from "react";
+import type { Card } from "@/generated/prisma/client";
+import dayjs from "@/lib/dayjs";
 import { createCard } from "../../api/cards";
+import classes from "./CardTable.module.css";
 
-const PLACEHOLDER_CONTENT = {
-	type: "text",
-	front: "this is the front of my card",
-	back: "this is the back of my cards",
-};
+// TEMPORARY HELPER FUNCTION TO CREATE CARDS FOR RESTING
+function randomText(prefix: string) {
+	const chars =
+		"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	let text = "";
+	const length = 8; // change this to make it longer
+
+	for (let i = 0; i < length; i++) {
+		text += chars.charAt(Math.floor(Math.random() * chars.length));
+	}
+
+	return `${prefix}: ${text}`;
+}
 
 type CardTableProps = {
 	deckId: string;
+	cards: Card[];
 };
 
-export const CardTable = ({ deckId }: CardTableProps) => {
+export const CardTable = ({ deckId, cards }: CardTableProps) => {
+	const [selection, setSelection] = useState<string[]>(["1"]);
+
 	const handleCreate = async () => {
-		const newCard = await createCard({
-			data: { deckId, content: PLACEHOLDER_CONTENT },
+		await createCard({
+			data: {
+				deckId,
+				content: {
+					type: "text",
+					front: randomText("Randomized front"),
+					back: randomText("Randomized back"),
+				},
+			},
 		});
-		console.log("created Card: ", newCard);
 	};
 
+	const toggleRow = (id: string) => {
+		setSelection((current) =>
+			current.includes(id)
+				? current.filter((item) => item !== id)
+				: [...current, id],
+		);
+	};
+
+	const toggleAll = () =>
+		setSelection((current) =>
+			current.length === cards.length ? [] : cards.map((item) => item.id),
+		);
+
+	const rows = cards.map((card) => {
+		const selected = selection.includes(card.id);
+
+		return (
+			<Table.Tr
+				key={card.id}
+				className={cx({ [classes.rowSelected]: selected })}
+			>
+				<Table.Td>
+					<Checkbox
+						checked={selection.includes(card.id)}
+						onChange={() => toggleRow(card.id)}
+					/>
+				</Table.Td>
+				<Table.Td>
+					<Text size="sm" fw={700}>
+						{card.content.type}
+					</Text>
+				</Table.Td>
+				<Table.Td>
+					<Text size="sm">{card.level}</Text>
+				</Table.Td>
+				<Table.Td>
+					<Text size="sm">{dayjs(card.reviewAt).fromNow()}</Text>
+				</Table.Td>
+				{card.content.type === "text" && (
+					<Table.Td>
+						<Text size="xs" fw={700}>
+							FRONT
+						</Text>
+						<Text size="sm" pb="lg">
+							{card.content.front}
+						</Text>
+
+						<Text size="xs" fw={700}>
+							BACK
+						</Text>
+						<Text size="sm">{card.content.back}</Text>
+					</Table.Td>
+				)}
+			</Table.Tr>
+		);
+	});
+
 	return (
-		<Group>
+		<Stack h={"100%"} className={classes.wrapper}>
 			<Button onClick={handleCreate}>Create new Card with random values</Button>
-		</Group>
+
+			<Table verticalSpacing="sm">
+				<Table.Thead>
+					<Table.Tr>
+						<Table.Th w={50}>
+							<Checkbox
+								onChange={toggleAll}
+								checked={selection.length === cards.length}
+								indeterminate={
+									selection.length > 0 && selection.length !== cards.length
+								}
+							/>
+						</Table.Th>
+						<Table.Th w={100}>Type</Table.Th>
+						<Table.Th w={100}>Level</Table.Th>
+						<Table.Th w={150}>Review</Table.Th>
+						<Table.Th miw={400} ta="center">
+							Content
+						</Table.Th>
+					</Table.Tr>
+				</Table.Thead>
+				<Table.Tbody>{rows}</Table.Tbody>
+			</Table>
+		</Stack>
 	);
 };
--- src/lib/dayjs.ts
+++ src/lib/dayjs.ts
@@ -3,12 +3,34 @@ import isoWeek from "dayjs/plugin/isoWeek";
 import isSameOrAfter from "dayjs/plugin/isSameOrAfter";
 import isSameOrBefore from "dayjs/plugin/isSameOrBefore";
 import isToday from "dayjs/plugin/isToday";
+import relativeTime from "dayjs/plugin/relativeTime";
+import updateLocale from "dayjs/plugin/updateLocale";
 import utc from "dayjs/plugin/utc";
 
+dayjs.extend(updateLocale);
 dayjs.extend(isToday);
+dayjs.extend(relativeTime);
 dayjs.extend(isSameOrAfter);
 dayjs.extend(isSameOrBefore);
 dayjs.extend(isoWeek);
 dayjs.extend(utc);
+
+dayjs.updateLocale("en", {
+	relativeTime: {
+		future: "in %s",
+		past: "now",
+		s: "a few seconds",
+		m: "a minute",
+		mm: "%d minutes",
+		h: "an hour",
+		hh: "%d hours",
+		d: "a day",
+		dd: "%d days",
+		M: "a month",
+		MM: "%d months",
+		y: "a year",
+		yy: "%d years",
+	},
+});
 
 export default dayjs;
