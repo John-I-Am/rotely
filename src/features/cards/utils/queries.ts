1cde97c28d790624481a30265a1bbc49ddb2d068
John <47208192+John-I-Am@users.noreply.github.com>
2025-10-09 00:49:55 +1300

    feat(cards): add utility function to filter due cards

--- src/features/cards/utils/queries.ts
+++ src/features/cards/utils/queries.ts
@@ -1,6 +1,16 @@
 import type { DeckWithCards } from "@/features/decks/types";
+import type { Card } from "@/generated/prisma/client";
+import dayjs from "@/lib/dayjs";
 
 export const getAllCardsFromDecks = (decks: DeckWithCards[]) => {
 	const cards = decks.flatMap((deck: DeckWithCards) => deck.cards);
 	return cards;
+};
+
+export const filterCardsDue = (cards: Card[]) => {
+	const cardsDue: Card[] = cards.filter((card: Card) =>
+		dayjs(card.reviewAt).isSameOrBefore(dayjs()),
+	);
+
+	return cardsDue;
 };
